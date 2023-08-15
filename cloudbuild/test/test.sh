79523cbd03da517d2894e65801231579eb9a995c
Fernando Arreola <figutierrez@google.com>
2023-08-15 16:30:01 -0700

    Update test script.

--- cloudbuild/test/test.sh
+++ cloudbuild/test/test.sh
@@ -3,9 +3,6 @@ set -euxo pipefail
 
 nix-shell --quiet --pure --command "$(cat <<NIXCMD
   set -euxo pipefail
-  export PGHOST=127.0.0.1
-  export PGDATABASE=postgres
-  export PGUSER=673673622326@cloudbuild
   cd /workspace
   npm ci --silent
   npm run lint && npm run build && npm run test-silent
